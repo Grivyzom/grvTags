6109d769bf6eacd5f031b037747ab3bfecddbb2b
Brocolitx <grivyzom@gmail.com>
2025-06-09 00:22:10 -0400

    add:
    Se añaden los archivos:
    
    - grvTags
    - plugin.yml
    - tags.yml
    - categories.yml

---
+++ src/main/java/gc/grivyzom/grvTags.java
@@ -0,0 +1,15 @@
+package gc.grivyzom;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class grvTags extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // Código de inicialización
+    }
+
+    @Override
+    public void onDisable() {
+        // Código de limpieza
+    }
+}
